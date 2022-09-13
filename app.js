c54200d8e015d8164b159fa8bcb026b99f439723
ivan2798 <inge1998@hotmail.com>
2022-09-13 18:49:37 -0500

    random color

--- app.js
+++ app.js
@@ -15,18 +15,57 @@ console.log(rutaCompletaLiteral);
 
 //DOM
 
-document.getElementById("test").innerHTML = 
-`<a href="${rutaCompletaLiteral}">${rutaCompletaLiteral}</a>`
+/*document.getElementById("test").innerHTML = 
+`<a href="${rutaCompletaLiteral}">${rutaCompletaLiteral}</a>`*/
  //tope y busca test
 
 
+
+/*
+console.log(rgb);
+console.log(rgbLiteral);
+*/
+/*document.getElementById("test").style.backgroundColor = rgbLiteral //rgb(0,0,0)*/
+
+//otra manera
+/*
 let r=0;
-let g=0;
-let b=0;
+let g=255;
+let b=23;
 
 let rgb = "rgb("+r+","+g+","+b+")";
 
 let rgbLiteral = `rgb(${r},${g},${b})`;
+document.body.innerHTML = `
+<div id="test" style="background-color:rgb(${r},${g},${b})"> </div>
+`
+*/
 
-console.log(rgb);
-console.log(rgbLiteral);
+//cambio de colores aleatorio
+
+//definir una funcion
+//esta funcion va a generar 3 numeros aleatorios
+//estos 3 numeros van a añadirse a
+
+
+//Arrow functions
+const suma = (a,b) => {
+    return a + b;
+}
+
+console.log(suma(4,5));
+
+const randomColor = () => {
+    let r = Math.floor(Math.random()*256);//aleatorio
+    let g = Math.floor(Math.random()*256);//aleatorio
+    let b = Math.floor(Math.random()*256);//aleatorio
+    let divTest = `
+    <div id="test" style="background-color:rgb(${r},${g},${b})"> </div>
+    `;
+
+    return divTest;
+
+}
+
+document.body.innerHTML = randomColor();
+
