702f6af3e9ad9fb5a3dabdd24c55ebcc0d1f4dbe
ivan2798 <inge1998@hotmail.com>
2022-09-13 18:25:22 -0500

    templates

--- app.js
+++ app.js
@@ -12,3 +12,21 @@ let rutaCompletaLiteral = `${url}/${ruta}`;
 console.log(rutaCompleta);
 
 console.log(rutaCompletaLiteral);
+
+//DOM
+
+document.getElementById("test").innerHTML = 
+`<a href="${rutaCompletaLiteral}">${rutaCompletaLiteral}</a>`
+ //tope y busca test
+
+
+let r=0;
+let g=0;
+let b=0;
+
+let rgb = "rgb("+r+","+g+","+b+")";
+
+let rgbLiteral = `rgb(${r},${g},${b})`;
+
+console.log(rgb);
+console.log(rgbLiteral);
