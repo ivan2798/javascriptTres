bef62a12022252192a626b7b437d3d44d0519dea
ivan2798 <inge1998@hotmail.com>
2022-09-13 18:10:02 -0500

    DOM

---
+++ app.js
@@ -0,0 +1,14 @@
+//DOM - Document Object Model
+
+//String Literals - Template literals // Cadenas literals
+
+let url = "https://www.cuttevents.online";
+let ruta = "experiencia-online";
+
+let rutaCompleta = url + "/" + ruta;
+
+let rutaCompletaLiteral = `${url}/${ruta}`;
+
+console.log(rutaCompleta);
+
+console.log(rutaCompletaLiteral);
